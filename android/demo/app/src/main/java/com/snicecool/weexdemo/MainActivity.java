96d48565aab5cea8c985ee9f1c18510b8ffac24e
zhuzhe <zhuzheteng@gmail.com>
2017-01-24 14:19:58 +0800

    增加demo

--- android/demo/app/src/main/java/com/snicecool/weexdemo/MainActivity.java
+++ android/demo/app/src/main/java/com/snicecool/weexdemo/MainActivity.java
@@ -1,13 +1,94 @@
 package com.snicecool.weexdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.taobao.weex.IWXRenderListener;
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.common.WXRenderStrategy;
+
+public class MainActivity extends AppCompatActivity implements IWXRenderListener {
+
+    WXSDKInstance mWXSDKInstance;
+    final static String START_PAGE = "https://www.zhuzhe.wang/examples/build/vue/index.js";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mWXSDKInstance = new WXSDKInstance(this);
+        mWXSDKInstance.registerRenderListener(this);
+//        mWXSDKInstance.render("WXSample", WXFileUtils.loadAsset("app.weex.js", this), null, null, -1, -1, WXRenderStrategy.APPEND_ASYNC);
+        mWXSDKInstance.renderByUrl("Vue首页", START_PAGE, null, null, WXRenderStrategy.APPEND_ASYNC);
+    }
+
+    @Override
+    public void onViewCreated(WXSDKInstance instance, View view) {
+        setContentView(view);
+    }
+
+    @Override
+    public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
+
+    }
+
+    @Override
+    public void onRefreshSuccess(WXSDKInstance instance, int width, int height) {
+
+    }
+
+    @Override
+    public void onException(WXSDKInstance instance, String errCode, String msg) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                mWXSDKInstance.renderByUrl("Vue首页", START_PAGE, null, null, WXRenderStrategy.APPEND_ASYNC);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityPause();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityStop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityDestroy();
+        }
     }
 }
